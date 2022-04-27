177f70028aa2543450c7781018f765664899a323
Mads942n <72200389+Mads942n@users.noreply.github.com>
2022-04-27 15:40:32 +0200

    Setup
    Basic JS fetch, added sections and divs to home.html

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ fetchPromise
     })
     .then( json =>{
 
-        console.log(json[0])
+        let output;
+
+        json.forEach(i => {
+            console.log(i.productname)
+            output += `<div class="div--card">
+            <img src="${i.picture}" alt="">
+            <p class="div--card__info">${i.description_short}</p>
+            <h3 class="div-card__price">${i.price}</h3><img src="" alt="Add to cart">
+            </div>`
+
+        });
+        document.querySelector('.section--cards').innerHTML = output
+
 
     })
     .catch( error => {
